7a3c608a38b0fa5921df2a000924d7192a1da2a3
Manuel Duarte <carlosd99m@gmail.com>
2024-12-15 23:29:29 -0600

    feat: implement dependency injection with GetIt

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,1 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+import '../../features/auth/data/repositories/auth_repository.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+
+  getIt.registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryImpl(getIt<FirebaseAuth>()));
+}
